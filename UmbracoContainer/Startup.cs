44191a266b32f8e87432326001e00f41b83655b9
Alexander Seeliger <alexander@brightbits.de>
2022-10-22 18:01:25 +0200

    chore: cleanup startup

--- UmbracoContainer/Startup.cs
+++ UmbracoContainer/Startup.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Umbraco.Cms.Core.DependencyInjection;
-using Umbraco.Extensions;
 
 namespace UmbracoContainer
 {
@@ -20,7 +18,7 @@ namespace UmbracoContainer
         /// <param name="webHostEnvironment">The web hosting environment.</param>
         /// <param name="config">The configuration.</param>
         /// <remarks>
-        /// Only a few services are possible to be injected here https://github.com/dotnet/aspnetcore/issues/9337
+        /// Only a few services are possible to be injected here https://github.com/dotnet/aspnetcore/issues/9337.
         /// </remarks>
         public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
         {
@@ -34,18 +32,15 @@ namespace UmbracoContainer
         /// <param name="services">The services.</param>
         /// <remarks>
         /// This method gets called by the runtime. Use this method to add services to the container.
-        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940.
         /// </remarks>
         public void ConfigureServices(IServiceCollection services)
         {
-#pragma warning disable IDE0022 // Use expression body for methods
             services.AddUmbraco(_env, _config)
                 .AddBackOffice()
                 .AddWebsite()
                 .AddComposers()
                 .Build();
-#pragma warning restore IDE0022 // Use expression body for methods
-
         }
 
         /// <summary>
